608dab61e52217125745496a17165836daaee30c
CSancho <carlos.sancho@estudiantat.upc.edu>
2025-10-17 14:05:47 +0200

    Cerca BFS implementada

--- src/main/java/edu/epsevg/prop/ac1/cerca/CercaBFS.java
+++ src/main/java/edu/epsevg/prop/ac1/cerca/CercaBFS.java
@@ -2,15 +2,67 @@ package edu.epsevg.prop.ac1.cerca;
 
 import edu.epsevg.prop.ac1.model.*;
 import edu.epsevg.prop.ac1.resultat.ResultatCerca;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
 
 public class CercaBFS extends Cerca {
+    
     public CercaBFS(boolean usarLNT) { super(usarLNT); }
 
     @Override
     public void ferCerca(Mapa inicial, ResultatCerca rc) {
+        //Node(Mapa estat, Node pare, Moviment accio, int depth, int g) g es cost acumulat
+        boolean fin = false;
         
+        Queue<Node> LNO = new LinkedList<>();
+        Set<Mapa> LNT = new HashSet<>();
+        
+        Node NodeInicial = new Node(inicial, null, null, 0, 0);
+        LNO.add(NodeInicial);
+        LNT.add(inicial);
+        
+        while(!LNO.isEmpty() && !fin){
+            Node actual = LNO.poll();
+            rc.incNodesExplorats();
+            rc.updateMemoria(LNO.size() + LNT.size());
+            if (actual.estat.esMeta()){
+                fin = true;
+                rc.setCami(reconstruirCami(actual));
+            }
+            else{
+                List <Moviment> ListaMovimientos = new ArrayList<>(actual.estat.getAccionsPossibles());
+                for (Moviment mov: ListaMovimientos){ //añado todos los mapas a partir de los movimientos posibles
+                    Mapa movido = actual.estat.mou(mov); 
+                    if (!LNT.contains(movido)){
+                        LNT.add(movido);
+                        LNO.add(new Node(movido, actual, mov, actual.depth+1, actual.g+1) ); 
+                    }
+                    else{
+                        rc.incNodesTallats();
+                    }
+                }
+            }
 
+        }
+        
+    }
+    
+    
+    private List<Moviment> reconstruirCami(Node node){
+        List<Moviment> cami = new ArrayList<>();
+        Node actual = node;
+        while (actual.pare != null){
+            cami.add(actual.accio);
+            actual = actual.pare;
+        }
+        Collections.reverse(cami);
+        return cami;
     }
    
 }
