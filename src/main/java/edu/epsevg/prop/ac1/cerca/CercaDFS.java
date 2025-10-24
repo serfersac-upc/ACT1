c158a03eda1b7a8f91aa5a8016a411ce985abae9
sergio <sergio@Victus.Sergio>
2025-10-24 09:43:35 +0200

    Test 1

--- src/main/java/edu/epsevg/prop/ac1/cerca/CercaDFS.java
+++ src/main/java/edu/epsevg/prop/ac1/cerca/CercaDFS.java
@@ -3,11 +3,61 @@ package edu.epsevg.prop.ac1.cerca;
 import edu.epsevg.prop.ac1.model.*;
 import edu.epsevg.prop.ac1.resultat.ResultatCerca;
 
+import java.util.*;
+
 public class CercaDFS extends Cerca {
     public CercaDFS(boolean usarLNT) { super(usarLNT); }
 
     @Override
     public void ferCerca(Mapa inicial, ResultatCerca rc) {
-     
+        Node arrel = new Node(inicial, null, null, 0, 0);
+        Set<Mapa> visitats = new HashSet<>();
+
+        Node solucio = dfs(arrel, rc, visitats);
+        if (solucio != null) {
+            rc.setCami(reconstruirCami(solucio));
+        }
     }
+    
+    
+    private Node dfs(Node node, ResultatCerca rc, Set<Mapa> visitats) {
+        rc.incNodesExplorats();
+
+        if (node.estat.esMeta()) return node;
+
+        if (usarLNT) {
+            // Si ja hem visitat aquest estat a menor profunditat, el tallem
+            if (visitats.contains(node.estat)) {
+                rc.incNodesTallats();
+                return null;
+            }
+            visitats.add(node.estat);
+        }
+
+        for (Moviment m : node.estat.getAccionsPossibles()) {
+            Mapa nouEstat = node.estat.mou(m);
+            Node fill = new Node(nouEstat, node, m, node.depth + 1, node.g + 1);
+
+            if (!usarLNT && estaEnBranca(node, nouEstat)) {
+                rc.incNodesTallats();
+                continue;
+            }
+
+            Node sol = dfs(fill, rc, visitats);
+            if (sol != null) return sol;
+        }
+
+        return null;
+    }
+
+    private boolean estaEnBranca(Node node, Mapa estat) {
+        Node actual = node;
+        while (actual != null) {
+            if (actual.estat.equals(estat)) return true;
+            actual = actual.pare;
+        }
+        return false;
+    }
+    
+    
 }
--- src/main/java/edu/epsevg/prop/ac1/cerca/CercaIDS.java
+++ src/main/java/edu/epsevg/prop/ac1/cerca/CercaIDS.java
@@ -4,12 +4,68 @@ package edu.epsevg.prop.ac1.cerca;
 import edu.epsevg.prop.ac1.model.*;
 import edu.epsevg.prop.ac1.resultat.ResultatCerca;
 
+import java.util.*;
+
 
 public class CercaIDS extends Cerca {
     public CercaIDS(boolean usarLNT) { super(usarLNT); }
 
     @Override
     public void ferCerca(Mapa inicial, ResultatCerca rc) {
-        
+        int limit = 0;
+        Node solucio;
+        boolean trobat = false;
+
+        while (!trobat) {
+            Set<Mapa> visitats = new HashSet<>();
+            Node arrel = new Node(inicial, null, null, 0, 0);
+
+            solucio = dfsLimitat(arrel, limit, rc, visitats);
+            if (solucio != null) {
+                rc.setCami(reconstruirCami(solucio));
+                trobat = true;
+            }
+            limit++;
+        }
+    }
+    private Node dfsLimitat(Node node, int limit, ResultatCerca rc, Set<Mapa> visitats) {
+        rc.incNodesExplorats();
+
+        if (node.estat.esMeta()) return node;
+        if (node.depth == limit) {
+            rc.incNodesTallats();
+            return null;
+        }
+
+        if (usarLNT) {
+            if (visitats.contains(node.estat)) {
+                rc.incNodesTallats();
+                return null;
+            }
+            visitats.add(node.estat);
+        }
+
+        for (Moviment m : node.estat.getAccionsPossibles()) {
+            Mapa nouEstat = node.estat.mou(m);
+            if (!usarLNT && estaEnBranca(node, nouEstat)) {
+                rc.incNodesTallats();
+                continue;
+            }
+
+            Node fill = new Node(nouEstat, node, m, node.depth + 1, node.g + 1);
+            Node sol = dfsLimitat(fill, limit, rc, visitats);
+            if (sol != null) return sol;
+        }
+
+        return null;
+    }
+
+    private boolean estaEnBranca(Node node, Mapa estat) {
+        Node actual = node;
+        while (actual != null) {
+            if (actual.estat.equals(estat)) return true;
+            actual = actual.pare;
+        }
+        return false;
     }
 }
